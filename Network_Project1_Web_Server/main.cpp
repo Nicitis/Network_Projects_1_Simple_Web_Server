b33e3764e3f0aa8bcd5178ac7139ec0a53060e04
Nicitis <kuala2038@gmail.com>
2021-10-11 15:55:20 +0900

    Fixed a small bug and add status code
    - Fixing small bug that cannot play the music in the index page.
    - Add status code and failure messages.

--- Network_Project1_Web_Server/main.cpp
+++ Network_Project1_Web_Server/main.cpp
@@ -10,26 +10,28 @@
 #include <string.h>
 
 #define DEFAULT_SIZE 4096
+#define STATUS_LENGTH 40
+#define MIME_LENGTH 40
+#define SERVER_ERROR_MSG "500 Internal server errors occur."
+#define PAGE_NOT_FOUND "400 Page not found."
 
 #pragma comment (lib, "ws2_32.lib")
 
-void GetFileName(char*, const char*, const char*, size_t);
-int PrintAllContentType(char* const, char* const, int);
+int InitializeWinsock();
+void BindSock(SOCKET, int);
+void GetLocalURI(char*, const char*, const char*, size_t);
+void GetMimeType(char* const, char* const);
+void GetStatusText(char* statusText, int statusCode);
+int FillHeader(SOCKET, char*, const char*, int, int);
+void SendFailMsg(SOCKET, int);
 void* HandleHttp(void*);
 
 
 int main()
 {
-	// 1. Initialize winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
-
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0)
-	{
-		perror("Can't Initialize winsock! Quitting\n");
+	// Initialize winsock
+	if (InitializeWinsock() == 0)
 		return 0;
-	}
 
 	// Create a socket
 	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
@@ -38,36 +40,29 @@ int main()
 		perror("Can't create a socket! Quitting\n");
 		return 0;
 	}
-
-	// Bind the ip address and port to a socket
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
-
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
+	
+	// Bind socket
+	BindSock(listening, 54000);
 
 	// Tell Winsock the socket is for listening
 	listen(listening, SOMAXCONN);
 
 	sockaddr_in client;
 	int clientSize = sizeof(client);
-
 	SOCKET clientSocket;
 
-	// 이제 연결 요청이 들어오면 accept로 client socket을 생성한 다음 별도 스레드에서 이를 처리(httpHandle)하도록 수정한다.
+	// 연결 요청이 들어오면 accept로 client socket을 생성한 다음 별도 스레드에서 이를 처리(httpHandle)한다.
 	while (1)
 	{
 		// Wait for a connection
 		clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-
 		pthread_t p_thread;
-
 		pthread_create(&p_thread, NULL, HandleHttp, (void*)clientSocket);
 	}
 
-	if (shutdown(clientSocket, SD_BOTH) != 0) {
-		perror("shutdown 오류\n");
+	if (shutdown(clientSocket, SD_BOTH) != 0)
+	{
+		perror("shutdown\n");
 	}
 	
 	// Close listening socket
@@ -79,7 +74,34 @@ int main()
 	return 0;
 }
 
-void GetFileName(char* fileNamePtr, const char* directory, const char* recvBuf, size_t sizeOfFileBuf)
+int InitializeWinsock()
+{
+	// Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		perror("Can't Initialize winsock! Quitting\n");
+		return 0;
+	}
+	return 1;
+}
+
+void BindSock(SOCKET listeningSock, int port)
+{
+	// Bind the ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+
+	bind(listeningSock, (sockaddr*)&hint, sizeof(hint));
+}
+
+// 상대 경로명을 가져온다.
+void GetLocalURI(char* fileURI, const char* directory, const char* recvBuf, size_t sizeOfFileBuf)
 {
 	// 요구하는 파일 경로 분석
 	int indexOfEnd, length = strlen(recvBuf), spaceCount = 0;
@@ -95,65 +117,134 @@ void GetFileName(char* fileNamePtr, const char* directory, const char* recvBuf, 
 	indexOfEnd--;
 	if (indexOfEnd == 4) // 아무것도 입력하지 않으면 index.html을 불러온다.
 		//sprintf_s(fileNamePtr, sizeOfFileBuf, "%s\\index.html", directory);
-		sprintf_s(fileNamePtr, sizeOfFileBuf, "%s%s", directory, "index.html");
+		sprintf_s(fileURI, sizeOfFileBuf, "%s%s", directory, "index.html");
 	else
 	{
-		int count = sprintf_s(fileNamePtr, sizeOfFileBuf, "%s", directory);
-		strncpy_s(fileNamePtr + count, sizeOfFileBuf - count, recvBuf + 5, (size_t)(indexOfEnd)-4);
+		int count = sprintf_s(fileURI, sizeOfFileBuf, "%s", directory);
+		strncpy_s(fileURI + count, sizeOfFileBuf - count, recvBuf + 5, (size_t)(indexOfEnd)-4);
 	}
 }
 
-int PrintContentTypeInnerFunc(char* const msgBuf, const char* mimeType, int indexOfMsg)
+void GetMimeType(char* const mimeType, char* const fileURI)
 {
-	return sprintf_s(msgBuf + indexOfMsg, (size_t)DEFAULT_SIZE - indexOfMsg - 1, "Content-Type: %s\r\n", mimeType);
-}
-
-int PrintAllContentType(char* const msgBuf, char* const fileURL, int indexOfMsg)
-{
-	int count;
-	char* type = strchr(fileURL, '.');
-	// 현재 서비스 타입: .txt, .html, .htm, .jpeg, .jpg, .gif
+	char* type = strchr(fileURI, '.');
+	// 현재 서비스 타입: .txt, .html, .htm, .jpeg, .jpg, .gif, .mp3, .mp4, .wav
 	if (strcmp(type, ".txt") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "text/plain", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "text/plain");
 	}
 	else if (strcmp(type, ".html") == 0 || strcmp(type, ".htm") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "text/html", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "text/html");
 	}
 	else if (strcmp(type, ".jpeg") == 0 || strcmp(type, ".jpg") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "image/jpeg", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "image/jpeg");
 	}
 	else if (strcmp(type, ".png") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "image/png", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "image/png");
 	}
 	else if (strcmp(type, ".gif") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "image/gif", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "image/gif");
 	}
 	else if (strcmp(type, ".mp3") == 0 || strcmp(type, ".mp4") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "audio/mpeg", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "audio/mpeg");
 	}
 	else if (strcmp(type, ".wav") == 0)
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "audio/x-wav", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "audio/x-wav");
 	}
 	else
 	{
-		count = PrintContentTypeInnerFunc(msgBuf, "application/octet-stream", indexOfMsg);
+		strcpy_s(mimeType, MIME_LENGTH, "application/octet-stream");
+	}
+}
+
+void GetStatusText(char* statusText, int statusCode)
+{
+	switch (statusCode)
+	{
+		case 200:
+			strcpy_s(statusText, STATUS_LENGTH, "200 OK");
+			break;
+		case 404:
+			strcpy_s(statusText, STATUS_LENGTH, "404 Not Found");
+			break;
+		case 500:
+			strcpy_s(statusText, STATUS_LENGTH, "500 Internal Server Error");
+			break;
+	}
+}
+
+int FillHeader(SOCKET clientSock, char* msgBuf, const char* mimeType, int statusCode, int fileSize)
+{
+	// status code 획득
+	char statusText[STATUS_LENGTH];
+	int length;
+	const char* header_fmt =
+		"HTTP/1.1 %s\r\n"
+		"Content-Length: %d\r\n"
+		"Content-Type: %s\r\n"
+		"Connection: keep-alive\r\n"
+		"\r\n";
+	char* contentType;
+
+	GetStatusText(statusText, statusCode);
+
+	length = sprintf_s(msgBuf, DEFAULT_SIZE,
+		"HTTP/1.1 %s\r\n"
+		"Content-Length: %d\r\n"
+		"Content-Type: %s\r\n"
+		"Connection: keep-alive\r\n"
+		"\r\n",
+		statusText,
+		fileSize,
+		mimeType);
+
+	return length;
+}
+
+void SendFailMsg(SOCKET clientSock, int status)
+{
+	char msgBuf[DEFAULT_SIZE], body[DEFAULT_SIZE];
+	int bytesToSend, headerLength, bodyLength;
+
+	switch (status)
+	{
+		case 404:
+			strcpy_s(body, DEFAULT_SIZE, PAGE_NOT_FOUND);
+			break;
+		case 500:
+			strcpy_s(body, DEFAULT_SIZE, SERVER_ERROR_MSG);
+			break;
+		default:
+			body[0] = '\0';
+			break;
 	}
 
-	// 반납 타입: 글자 수
-	return count;
+	bodyLength = strlen(body);
+	headerLength = FillHeader(clientSock, msgBuf, "text/html", status, bodyLength);
+	bytesToSend = headerLength + bodyLength + 1;
+
+	memcpy_s(msgBuf + headerLength, (size_t)DEFAULT_SIZE, body, bodyLength);
+	_snprintf_s(msgBuf + bytesToSend - 1, 1, 1, "\0");
+
+	int iResult = send(clientSock, msgBuf, bytesToSend, 0);
+
+	if (iResult == SOCKET_ERROR)
+	{
+		perror("Error in send(). Quitting\n");
+	}
+	closesocket(clientSock);
 }
 
 void* HandleHttp(void* arg)
 {
 	SOCKET clientSock = (SOCKET)arg;
-	char buf[DEFAULT_SIZE], fileName[DEFAULT_SIZE], *fbuf, *msgBuf;
+	char buf[DEFAULT_SIZE], localURI[DEFAULT_SIZE], *fbuf, *msgBuf;
 	const char* directory = "contents\\";
 	int index;
 
@@ -164,7 +255,7 @@ void* HandleHttp(void* arg)
 	if (bytesReceived == SOCKET_ERROR)
 	{
 		perror("Error in recv(). Quitting\n");
-		closesocket(clientSock);
+		SendFailMsg(clientSock, 500); // 500
 		return NULL;
 	}
 
@@ -173,21 +264,21 @@ void* HandleHttp(void* arg)
 	if (strstr(buf, "GET") == buf) // GET
 	{
 		int fileSize, headerLength;
-		ZeroMemory(fileName, DEFAULT_SIZE);
-		GetFileName(fileName, directory, buf, DEFAULT_SIZE); // 파일 이름 획득
+		ZeroMemory(localURI, DEFAULT_SIZE);
+		GetLocalURI(localURI, directory, buf, DEFAULT_SIZE); // 파일 이름 획득
 
-		printf("==requested fileName = %s...==\n\n", fileName);
+		//printf("==requested fileName = %s...==\n\n", fileName);
 
 		// 파일을 읽고 fbuf에 출력
 		FILE* fp;
-		fopen_s(&fp, fileName, "rb");
+		fopen_s(&fp, localURI, "rb");
 
 		if (fp == NULL)
 		{
 			// 파일 열기 실패
 			perror("cannot open the file...\n\n");
-			closesocket(clientSock);
-			return NULL; // 수정 요함
+			SendFailMsg(clientSock, 404); // 404 NOT FOUND
+			return NULL;
 		}
 
 		fseek(fp, 0, SEEK_END);
@@ -200,29 +291,34 @@ void* HandleHttp(void* arg)
 		{
 			// 에러 발생
 			perror("error occurs\n");
-			closesocket(clientSock);
+			SendFailMsg(clientSock, 500); // 500
+			free(msgBuf);
+			fclose(fp);
 			return NULL;
 		}
 
 		ZeroMemory(msgBuf, DEFAULT_SIZE);
 
 		// Header
-		index = sprintf_s(msgBuf, DEFAULT_SIZE, "HTTP/1.1 200 OK\r\n"); // 상태 메시지
-		index += sprintf_s(msgBuf + index, (size_t)DEFAULT_SIZE - index - 1,
-			"Content-Length: %d\r\n", fileSize);						// 본문의 길이		
-		index += PrintAllContentType(msgBuf, fileName, index);			// 본문의 형식
-		index += sprintf_s(msgBuf + index, (size_t)DEFAULT_SIZE - index - 1, 
-			"Connection: keep-alive\r\n\r\n");							// 연결 형식
-		headerLength = index;
+		char mimeType[MIME_LENGTH];
+		GetMimeType(mimeType, localURI);
+		headerLength = FillHeader(clientSock, msgBuf, mimeType, 200, fileSize);
 
 		// Body
 		fbuf = (char*)malloc(sizeof(char) * fileSize);
 
 		if (fbuf == NULL)
+		{
+			SendFailMsg(clientSock, 500); // 500
+			free(fbuf);
+			free(msgBuf);
+			fclose(fp);
 			return NULL;
+		}
 		fseek(fp, 0, SEEK_SET); // 위치 초기화
 		fread(fbuf, sizeof(char), fileSize, fp);
 
+		// Resize
 		int bytesToSend = headerLength + fileSize + 1;
 		if (bytesToSend > DEFAULT_SIZE)
 		{
@@ -230,17 +326,18 @@ void* HandleHttp(void* arg)
 			msgBuf = (char*)realloc(msgBuf, sizeof(char) * bytesToSend);
 			if (msgBuf == NULL)
 			{
+				// Fail to resize msg buffer.
+				SendFailMsg(clientSock, 500); // 500
 				free(fbuf);
 				free(oldBuf);
 				fclose(fp);
-				closesocket(clientSock);
 				return NULL;
 			}
 		}
 
-		// 문자열이 아닐 경우, 중간에 NULL 문자열이 있어서 formating 형식으로 복제하는 것으로는 부족할 수도 있다. 따라서 한 바이트 한 바이트씩 출력하는 것이 필요하다.
+		// Copy memory
 		memcpy_s(msgBuf + headerLength, (size_t)bytesToSend, fbuf, fileSize);
-		_snprintf_s(msgBuf + fileSize, 1, 1, "\0");
+		_snprintf_s(msgBuf + headerLength + fileSize, 1, 1, "\0");
 
 		int iResult = send(clientSock, msgBuf, bytesToSend, 0);
 
